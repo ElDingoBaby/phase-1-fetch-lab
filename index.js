e0be38d00989bf17fbe09db45a391be2c60e7fe4
Alexis Sanchez <alexiss574@gmail.com>
2023-02-28 12:37:28 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
+  return (fetch('https://anapioficeandfire.com/api/books'))
+    .then((response) => response.json())
+    .then(bookData => renderBooks(bookData))
+
   // To pass the tests, don't forget to return your fetch!
-  
+
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +17,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
